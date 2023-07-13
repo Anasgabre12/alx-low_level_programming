445f016416caf7331136e54c6bd50f489a3da6ac
Anasgabre12 <anasgabre4@gamil.com>
2023-07-13 15:47:21 -0700

    ./a.out

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
 	 * Description: A cprogra,that prints with printf funcation.
 	 *
 	 * Return: Always 0 (sucsses)
-	 */
+	*/
 	  int main(void)
 	{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
