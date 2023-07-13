c672334eb0b06f5de1a0535f5b2526555b6d3093
Anasgabre12 <anasgabre4@gamil.com>
2023-07-13 16:28:17 -0700

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * 
- *main - Entr point
+ * main - Entr point
  *
  * Descriotion: A C programe that puts  with puts funcation 
  *
- *Return: Always 0 (sucsess)
+ * Return: Always 0 (sucsess)
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts( "\"Programming is like building a multilingual puzzle");
 	return(0);
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/***/
+int main(void)
+{
+	printf("Size of a char:",sizeof(char));
+	printf("Size of an int:",sizeof(int));
+	printf("Size of a long:",sizeof(long));
+	printf("Size of a long long int:",sizeof(long long));
+	printf("Size of a float:",sizeof(float));
+	return(0)
+}
