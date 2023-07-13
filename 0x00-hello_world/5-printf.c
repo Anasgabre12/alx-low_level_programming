4ea762c7a513181f08e2f79e66b20c0e9cdc8922
Anasgabre12 <anasgabre4@gamil.com>
2023-07-13 15:32:32 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+	/**
+	 * main - Entry point 
+	 *
+	 * Description: A cprogra,that prints with printf funcation.
+	 *
+	 * Return:  Always 0 (sucsses)
+	 * \
+	 int main (void)
+	 {
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+	}
