520bc2737578e316ed29c87f89bce3315e828a17
Anasgabre12 <anasgabre4@gamil.com>
2023-07-13 15:21:00 -0700

    trailing whitespace

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-/*first c code*/
+/**
+ * first c code
+ * main entry point 
+ * Return: Alwayse 0 (sucsses)
+ */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
