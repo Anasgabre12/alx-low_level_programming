d0f827d4a886383bb44b6336063bd1b550ce4e12
Anasgabre12 <anasgabre4@gamil.com>
2023-07-14 11:22:22 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry alpha
+ * Return: Alwayse 0 (sucsess)
+*/
+int main(void)
+{
+	char ch = 'a'
+	char ch = 'A'
+	/*print a - z*/
+	while
+	(ch <= 'z');
+	{
+	putchar(ch);
+	ch++;
+	}
+
+	/*print A -Z*/
+	while
+	(ch <= 'Z');
+	{
+	putchar(ch);
+	ch++;
+	}
+	putchar('\n');
+	return (0);
+}
