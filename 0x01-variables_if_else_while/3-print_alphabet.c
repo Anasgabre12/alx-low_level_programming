305297eda1ba202dbf5d338bdbe656ad8cf7c937
Anasgabre12 <anasgabre4@gamil.com>
2023-07-14 12:27:53 -0700

    3-print_alphabet.c

--- 0x01-variables_if_else_while/3-print_alphabet.c
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -7,12 +7,20 @@ int main(void)
 {
 char c;
 c = 'a';
+c = 'A';
+/*print a - z*/
 while
 (c <= 'z')
 {
 putchar(c);
 c++;
 }
+/*print A - Z*/
+while
+(c <= 'Z')
+{
+putchar(c);
+c++
 putchar('\n');
 return (0);
 }
