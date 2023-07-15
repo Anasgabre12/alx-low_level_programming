6c96e9dee279366f566b2fc056c4a897d1340638
Anasgabre12 <anasgabre4@gamil.com>
2023-07-15 06:48:41 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-
-/***/
+/**
+ * main - Entr point
+ * Return: Always 0 (sucsess)
+*/
 int main(void)
 {
-	printf("Size of a char:" ,sizeof(char));
-	printf("Size of an int:" ,sizeof(int));
-	printf("Size of a long:" ,sizeof(long int));
-	printf("Size of a long long int:" ,sizeof(long long int));
-	printf("Size of a float:" ,sizeof(float));
-	return(0)
+
+printf("size of a char:", sizeof(char));
+printf("size of a int:", sizeof(int));
+printf("size of a long int:", sizeof(long int));
+printf("size of a long long int:", sizeof(long long int));
+printf("size of a float:", sizeof(float));
+return (0);
+
 }
