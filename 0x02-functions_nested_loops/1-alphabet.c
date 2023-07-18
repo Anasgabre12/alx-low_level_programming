177d6ba69a8d0e64a9c8bf944022111f43a5ce49
Anasgabre12 <anasgabre4@gamil.com>
2023-07-18 07:05:08 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,16 @@
 
 /**
  *  print_alphabet - prints the alphabet in lowercase, followed by a new line.
- *  Return: 0 (sucsess)
+ *  Return: Always 0 (sucsess)
 */
 void print_alphabet(void);
 {
-	char latter;
-	for
-		(latter = 'a', latter <= 'z', latter++);
-	_putchar(latter);
-	_putchar('\n')
+
+char latter;
+for
+
+(latter = 'a', latter <= 'z', latter++);
+_putchar(latter);
+_putchar('\n')
 }
 
