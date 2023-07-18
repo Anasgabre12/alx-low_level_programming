04ef2a67cefae8143992d66c227890dffa889097
Anasgabre12 <anasgabre4@gamil.com>
2023-07-18 06:48:16 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *  print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ *  Return: 0 (sucsess)
+*/
+void print_alphabet(void);
+{
+	char latter;
+	for
+		(latter = 'a', latter <= 'z', latter++);
+	_putchar(latter);
+	_putchar('\n')
+}
+
